1a6fbdfeabc84b0388a8a4f592e35939a537c23f
Adida <adida.barack@student.moringaschool.com>
2024-04-27 22:11:42 +0300

    In progres

--- src/components/datacontent.jsx
+++ src/components/datacontent.jsx
@@ -1,13 +1,16 @@
+import React from "react"
+import './style.css'
 export function TableStructure () {
     return (
         <div>
+             <link rel="stylesheet" href="styles.css"></link>
             <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
             <h3>The Royal Bank Of Flatiron</h3>
-            <div>
+            <div id='div1'>
                 <input type="Text" placeholder="Search Your Recent Transactions"/>
-                <i class='bx bx-search'></i>
+                <button><i class='bx bx-search'></i></button>
             </div>
-            <div>
+            <div id='div2'>
                 <input type="date" />
                 <input type="text" placeholder="Movies" />
                 <input type="text" placeholder="Cartegory"/>
